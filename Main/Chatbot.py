5a31502dfd0c03767800d7fda49a8c5253f7d74a
GeonwooVincentKim <kdsnop@gmail.com>
2020-04-11 23:28:40 +0900

    Test

--- Main/Chatbot.py
+++ Main/Chatbot.py
@@ -1,9 +1,3 @@
-# import telegram
-# kkw_token = '912709363:AAHx1AyZD1mfj-CQBDL-tUjk3nKfRHCZ568'
-# kkw = telegram.Bot(token=kkw_token)
-# updates = kkw.getUpdates()
-# for u in updates:
-#     print(u.message)
 import time
 
 import ChatBotModel
@@ -59,36 +53,8 @@ def print_menu(bot, update):
             exit(0)
 
 
-# def proc_rolling(bot, update):
-#     cjw.sendMessage("펑!!!")
-#     sound = firecracker()
-#     cjw.sendMessage(sound)
-#     cjw.sendMessage('쿠와아아앙ㄹ닝ㅁㄹㄴㅇㄹㄴ마렁니;ㅁㄹ나ㅣ러;')
-#
-#
-# def proc_너_미쳤냐(bot, update):
-#     cjw.sendMessage("지랄마")
-#     sound = firecracker()
-#     cjw.sendMessage(sound)
-#     cjw.sendMessage('너가 또라이잖여 병x아')
-#
-#
-# def proc_stop(bot, update):
-#     cjw.sendMessage("잠을 쳐자기 시작합니다.")
-#     cjw.stop()
-#
-#
-# def firecracker():
-#     return '쿠오아아ㅏ아앙!'
-
-#
 if __name__ == "__main__":
-    # print("test")
-    # print_menu()
     cjw = ChatBotModel.BotCK()
     cjw.add_handler('STTTAAARRT', print_menu)
     cjw.add_handler('WAIT', mMelon)
-    # # cjw.add_handler('rolling', proc_rolling)
-    # # cjw.add_handler('너_미쳤냐??', proc_너_미쳤냐)
-    # cjw.add_handler('stop', proc_stop)
     cjw.start()
